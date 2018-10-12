eae5f9cf48480984b5908d23cb3b547a6c23442f
momoka <454768497@qq.com>
2018-10-12 18:22:24 +0800

    买家商品DAO

---
+++ src/main/java/com/sn/sell/dataobject/ProductInfo.java
@@ -0,0 +1,31 @@
+package com.sn.sell.dataobject;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+/*
+商品
+ */
+@Entity
+@Data
+public class ProductInfo {
+    @Id
+    private String productId;
+    /* 名字*/
+    private String productName;
+    /*单价*/
+    private BigDecimal productPrice;
+    /*库存*/
+    private Integer productStock;
+    /*描述*/
+    private String productDescription;
+    /* 商品小图*/
+    private String productIcon;
+    /*商品状态 0正常 1下架*/
+    private Integer productStatus;
+    /*类目编号*/
+    private Integer categoryType;
+}
---
+++ src/main/java/com/sn/sell/repository/ProductInfoRepository.java
@@ -0,0 +1,12 @@
+package com.sn.sell.repository;
+
+import com.sn.sell.dataobject.ProductInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductInfoRepository extends JpaRepository<ProductInfo,String>{
+
+    List<ProductInfo> findByProductStatus(Integer productStatus);
+
+}
