303e9ebb9119ecd68cb1ae63f84397ce8aad1e26
momoka <454768497@qq.com>
2018-10-13 16:17:15 +0800

    买家商品Service

---
+++ src/main/java/com/sn/sell/service/ProductService.java
@@ -0,0 +1,21 @@
+package com.sn.sell.service;
+
+import com.sn.sell.dataobject.ProductInfo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface ProductService {
+    ProductInfo findOne(String productId);
+    /** 查询在架的商品列表**/
+    List<ProductInfo> findUpAll();
+
+    Page<ProductInfo> findAll(Pageable pageable);
+
+    ProductInfo save(ProductInfo productInfo);
+
+    //加库存
+
+    //减库存
+}
